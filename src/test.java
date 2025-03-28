2bc8f0f536acc1931d43b8cba6523d75aa22465d
W-Herb <3219332909@qq.com>
2025-03-28 10:25:10 +0800

    去除了一条注释

--- src/test.java
+++ src/test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String[] args) {
         char []str = {'a','p','p','l','e'};
-//测试commit
+
         for(int i = 0;i<str.length;i++){
             System.out.print(str[i]);
         }
